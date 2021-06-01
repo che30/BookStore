7c8c716d99fd0121ab58f026171f40d91d0bd388
che <che.blancoo58@gmail.com>
2021-06-01 12:24:16 +0100

    Add categories to form

--- src/containers/Bookform.js
+++ src/containers/Bookform.js
@@ -1,5 +1,7 @@
 const BookForm =()=>{
-<div>
+  const bookCataegories =["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
+  return(
+    <div>
   <form>
     <label> book name:</label>
     <input type="input" id="bkname"></input>
@@ -7,8 +9,15 @@ const BookForm =()=>{
     <input type="input" id="bknumber"></input>
     <label> number of pages:</label>
     <input type="input" id="pages"></input>
+    <select>
+     {bookCattaegories.each(cat=>{
+       <option key={cat} value= {cat}> </option>
+     })}
+    </select>
     <button type="submit"></button>
   </form>
 </div>
+  )
+
 }
 export  default BookForm
