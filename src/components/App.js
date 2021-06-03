2836fd6a623c4d3671ac1708bb70bb905741c2db
che <che.blancoo58@gmail.com>
2021-06-03 13:06:21 +0100

    Add Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 // import './App.css';
 import BookForm from '../containers/Bookform';
 import BookList from '../containers/Booklist';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BookList />
       <BookForm />
     </div>
